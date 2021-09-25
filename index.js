e9406a24cc10a6d27e873d348c9cc420c07c372d
DeividZavala <crusedmoss@gmail.com>
2021-09-25 16:13:08 -0500

    basic getting request meta data

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const http = require("http");
 const url = require("url");
 
 const server = http.createServer(function (req, res) {
+  //Getting the url and parsing it
   const parsedUrl = url.parse(req.url, true);
+
+  // Getting the path
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
+  // Getting the HTTP method
+  const method = req.method.toLocaleLowerCase();
+
+  // Getting query string object
+  const queryStringObject = parsedUrl.query;
+
   res.end("hello World!");
 
-  console.log(`path requested: ${trimmedPath}`);
+  console.log(
+    `path requested: ${trimmedPath} with method ${method} and these query string params ${queryStringObject}`
+  );
 });
 
 server.listen("3000", function () {
