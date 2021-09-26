dcc19244881a7fdbcf7087f52322ad8bd27c83bf
DeividZavala <crusedmoss@gmail.com>
2021-09-25 19:52:25 -0500

    routing requests handlers

--- index.js
+++ index.js
@@ -27,12 +27,55 @@ const server = http.createServer(function (req, res) {
   });
   req.on("end", () => {
     buffer += decoder.end();
-    res.end("hello World!");
 
-    console.log("payload:", buffer);
+    // Choose the handler this request should call, if one is not found, use the notFound handler.
+    const chosenHandler =
+      typeof router[trimmedPath] !== "undefined"
+        ? router[trimmedPath]
+        : handlers.notFound;
+
+    // Construct the data object to send to the handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer,
+    };
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, function (statusCode = 200, payload = {}) {
+      // Convert the payload to a string
+      const payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      console.log("Returned response:", statusCode, payloadString);
+    });
   });
 });
 
 server.listen("3000", function () {
   console.log("server is listening on port 3000");
 });
+
+// Define handlers
+
+const handlers = {
+  // Sample handler
+  sample(data, callback) {
+    // Callback a http status code, and payload object
+    callback(200, { name: "sample handler" });
+  },
+  // Not found handler
+  notFound(data, callback) {
+    callback(404);
+  },
+};
+
+// Define a request router
+const router = {
+  sample: handlers.sample,
+};
